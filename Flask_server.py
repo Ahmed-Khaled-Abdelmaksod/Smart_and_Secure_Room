d2c62f60a3996afe0943182fc8ae41ee15884c8d
ahmed-khaled-abdelmaksod <ak4902280@gmail.com>
2025-10-05 23:19:38 +0300

    Enhance Flask server with face verification and registration endpoints; update camera module for MQTT integration and server communication

--- Flask_server.py
+++ Flask_server.py
@@ -2,14 +2,20 @@ from flask import Flask,request,jsonify
 import json
 import numpy as np
 import cv2
+from faceRecognitionSystem import FaceRecognitionSystem
 
 app = Flask(__name__)
+system = FaceRecognitionSystem(threshold=0.6)
 
 @app.route("/",methods=['POST'])
-def root_function():
+def root_post_function():
     data = request.json
     print (data)
     return data
+
+@app.route("/",methods=['GET'])
+def root_get_function():
+    return "Hello Auth System :)"
 
 @app.route("/predict",methods=['POST'])
 def security_check():
@@ -28,6 +34,58 @@ def security_check():
     # cv2.waitKey(1)  # Wait until key press
     # cv2.destroyAllWindows()
     return jsonify({'Access': 'Auth'}) , 200
+
+
+@app.route("/verify", methods=["POST"])
+def verify_face():
+    if "image" not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    file = request.files["image"]
+    npimg = np.frombuffer(file.read(), np.uint8)
+    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    faces = system.detect_faces(frame)
+    if not faces:
+        return jsonify({"authorized": False, "reason": "No face detected"}), 200
+
+    x1, y1, x2, y2 = faces[0]
+    face_img = system.align_face(frame, (x1, y1, x2, y2))
+    name, similarity, authorized = system.recognize_face(face_img)
+    
+    return jsonify({"authorized": str(authorized),"name": str(name if authorized else None),"similarity": float(similarity)})
+
+
+# ========== REGISTER FACE ==========
+@app.route("/register", methods=["POST"])
+def register_face():
+    """
+    POST /register
+    Form-data:
+      - name: person's name
+      - image: uploaded image file
+    """
+    name = request.form.get("name")
+    if not name:
+        return jsonify({"error": "Missing 'name' parameter"}), 400
+
+    if "image" not in request.files:
+        return jsonify({"error": "No image uploaded"}), 400
+
+    file = request.files["image"]
+    npimg = np.frombuffer(file.read(), np.uint8)
+    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    faces = system.detect_faces(frame)
+    if not faces:
+        return jsonify({"error": "No face detected"}), 400
+
+    x1, y1, x2, y2 = faces[0]
+    face_img = system.align_face(frame, (x1, y1, x2, y2))
+    system.add_authorized_face(name, face_img)
+
+    return jsonify({"success": True, "message": f"Registered {name} successfully"})
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=7000)
--- camera.py
+++ camera.py
@@ -1,13 +1,19 @@
 import cv2
-import paho.mqtt.client as mqtt
+import paho.mqtt.client as mqtt 
 import requests
+import json
+
 
 BROKER = '65e275a531cf4f5eb698af1ff09c51a7.s1.eu.hivemq.cloud'
 PORT = 8883
 USER = 'hivemq.webclient.1758974031560'
 PASSWORD = 'n8!0I9&aNUF>Yw4zs<vM'
-FLASK_SERVER_URL = 'http://localhost:7000/predict'
-
+FLASK_SERVER_VERIFY_URL = 'http://localhost:7000/verify'
+FLASK_SERVER_REGISTER_URL = 'http://localhost:7000/register'
+camera_register_topic = 'room/camera/register'
+camera_verify_topic = 'room/camera/verify'
+REGISTER_CMD = 0
+VERIFY_CMD = 1
 client = mqtt.Client()
 client.username_pw_set(USER,PASSWORD)
 client.tls_set()
@@ -24,21 +30,30 @@ def capture_image():
         #     cv2.imshow("Preview", frame)
         #     cv2.waitKey(0)  # Wait until key press
         #     cv2.destroyAllWindows()
-def send_to_server(frame):
+def send_to_server(frame,cmd,name=None):
     ret,jpg = cv2.imencode('.jpg',frame)
     img_bytes = jpg.tobytes()
     files = {
         "image": ('capture.jpg', img_bytes, 'image/jpeg')
     }
-    resp = requests.post(FLASK_SERVER_URL, files=files, timeout=15)
-    print("Res:",resp.status_code," : ",resp.text)
+    data = {"name":'Ahmed'}
+    if cmd == REGISTER_CMD:
+        resp = requests.post(FLASK_SERVER_REGISTER_URL, files=files, data=data,timeout=15)
+        print("Res:",resp.status_code," : ",resp.text)
+    elif cmd == VERIFY_CMD:
+        resp = requests.post(FLASK_SERVER_VERIFY_URL, files=files, timeout=15)
+        print("Res:",resp.status_code," : ",resp.text)
 
 
 def on_message(client, userdata, msg):
-    print("message:",userdata)
+    print("message:",msg.payload)
     ret,frame = capture_image()
-    if ret:
-        send_to_server(frame)
+    if msg.topic == camera_register_topic :
+        if ret:
+            send_to_server(frame,REGISTER_CMD)
+    elif msg.topic == camera_verify_topic:
+        if ret:
+            send_to_server(frame,VERIFY_CMD)
 
 def on_connect(client, userdata, flags, rc, properties=None):
     print("connected :)")
@@ -47,9 +62,10 @@ def on_connect(client, userdata, flags, rc, properties=None):
 client.on_connect = on_connect
 client.on_message = on_message
 client.connect(BROKER,PORT)
-camera_topic = 'room/camera/capture'
 
-client.subscribe(camera_topic,qos=1)
+
+client.subscribe(camera_register_topic,qos=1)
+client.subscribe(camera_verify_topic,qos=1)
 
 
 
